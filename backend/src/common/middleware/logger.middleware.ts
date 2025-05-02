ae7902b38bc1bf6848314dc738d57b137e18afda
gogicha007 <gogicha@gmail.com>
2025-05-02 17:37:28 +0400

    fix: linter issue with logger.middleware

--- backend/src/common/middleware/logger.middleware.ts
+++ backend/src/common/middleware/logger.middleware.ts
@@ -2,22 +2,30 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { LoggingService } from 'src/common/services/logging.service';
 
+interface TypedRequestBody<T> extends Request {
+  body: T;
+}
+
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly loggingService: LoggingService) {}
 
-  use(req: Request, res: Response, next: NextFunction) {
+  use(
+    req: TypedRequestBody<Record<string, unknown>>,
+    res: Response,
+    next: NextFunction,
+  ) {
     const { method, url, query, body } = req;
     this.loggingService.info(
       `Incoming Request: ${method} ${url} - Query: ${JSON.stringify(query)} - Body: ${JSON.stringify(body)}`,
     );
 
     const originalSend = res.send;
-    res.send = (body: any) => {
+    res.send = (body: unknown): Response => {
       this.loggingService.info(
         `Response: ${res.statusCode} - Body: ${JSON.stringify(body)}`,
       );
-      return originalSend.call(res, body);
+      return originalSend.call(res, body) as Response;
     };
     next();
   }
