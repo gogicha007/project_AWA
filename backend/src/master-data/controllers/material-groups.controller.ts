c17f5137ca59eeb662f6d5f3702252871291b170
gogicha007 <gogicha@gmail.com>
2025-05-13 17:58:20 +0400

    chor: save the progress

--- backend/src/master-data/controllers/material-groups.controller.ts
+++ backend/src/master-data/controllers/material-groups.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MaterialGroupsService } from '../services/material-groups.service';
 import { CreateMaterualGroupDTO } from '../dto/createMaterialGroup.dto';
-CreateMaterualGroupDTO;
 
 @Controller('material-groups')
 export class MaterialGroupsController {
