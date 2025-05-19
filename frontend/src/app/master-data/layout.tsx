d561d9eca6200cb2d314d306376478d901b42bf4
gogicha007 <gogicha@gmail.com>
2025-05-19 10:11:28 +0400

    feat: highlight settings menu item when relevant path is open

--- frontend/src/app/master-data/layout.tsx
+++ frontend/src/app/master-data/layout.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import styles from './page.module.css';
 import React from 'react';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import { usePathname } from 'next/navigation';
 
 export default function MasterDataLayoutlayout({
   children,
@@ -9,19 +12,32 @@ export default function MasterDataLayoutlayout({
   children: React.ReactNode;
 }) {
   const tM = useTranslations('MasterData');
+  const pathname = usePathname();
   return (
     <section>
       <nav className={styles['settings__nav']}>
-        <Link href="/master-data/material-groups" className="button">
+        <Link
+          href="/master-data/material-groups"
+          className={`button ${pathname === '/master-data/material-groups' ? styles.activeLink : ''}`}
+        >
           {tM('material_groups.title')}
         </Link>
-        <Link href="/master-data/material-types" className="button">
+        <Link
+          href="/master-data/material-types"
+          className={`button ${pathname === '/master-data/material-types' ? styles.activeLink : ''}`}
+        >
           {tM('material_types.title')}
         </Link>
-        <Link href="/master-data/material-names" className="button">
+        <Link
+          href="/master-data/material-names"
+          className={`button ${pathname === '/master-data/material-names' ? styles.activeLink : ''}`}
+        >
           {tM('material_names.title')}
         </Link>
-        <Link href="/master-data/units" className="button">
+        <Link
+          href="/master-data/units"
+          className={`button ${pathname === '/master-data/units' ? styles.activeLink : ''}`}
+        >
           {tM('units.title')}
         </Link>
       </nav>
