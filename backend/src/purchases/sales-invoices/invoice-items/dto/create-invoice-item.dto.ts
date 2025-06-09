c5a016eafb76f62d8cd0bd316d8392f44c310305
gogicha007 <gogicha@gmail.com>
2025-06-09 21:49:11 +0400

    chor: save the progress

--- backend/src/purchases/sales-invoices/invoice-items/dto/create-invoice-item.dto.ts
+++ backend/src/purchases/sales-invoices/invoice-items/dto/create-invoice-item.dto.ts
@@ -1,10 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsNotEmpty,
-  IsString,
-  IsOptional,
-  IsInt,
-} from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional, IsInt } from 'class-validator';
 
 export class CreateInvoiceItemDTO {
   @ApiProperty()
