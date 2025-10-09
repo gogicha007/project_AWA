692d24e7ede2461de32b0e35d3b29e69ca87a927
gogicha007 <gogicha@gmail.com>
2025-10-09 17:50:38 +0400

    chor: save the progress

--- frontend/src/components/projects/project-boq/ProjectBoq.tsx
+++ frontend/src/components/projects/project-boq/ProjectBoq.tsx
@@ -1,7 +1,9 @@
+import FileUploader from "@/components/controls/file-uplo/FileUploader";
 export const ProjectBoq: React.FC<{ projectId: number }> = ({ projectId }) => {
   return (
     <div className="flex flex-col gap-4 p-6">
       <h2 className="text-lg font-semibold">Bill of Quantities (BoQ) for Project ID: {projectId}</h2>
+      <FileUploader />
     </div>
   );
 };
