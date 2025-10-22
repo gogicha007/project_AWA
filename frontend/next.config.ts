580afcbce0bb040382de0c77fda48d2c2f06b897
gogicha007 <gogicha@gmail.com>
2025-10-22 17:14:56 +0400

    chor: save the upgrade to next 16

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path/win32';
 
 const nextConfig: NextConfig = {
   turbopack: {
+    root: path.resolve(__dirname),
     rules: {
       '*.svg': {
         loaders: ['@svgr/webpack'],
