807cd51548e9a41af49d534f3b26108f7dbdd949
gogicha007 <gogicha@gmail.com>
2025-10-22 17:49:21 +0400

    chor: save the progress

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,28 +1,17 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals'
-
-// import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-// import nextTypescript from "eslint-config-next/typescript";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
-//   ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-// }];
-
+import nextVitals from 'eslint-config-next/core-web-vitals.js'
+ 
 const eslintConfig = defineConfig([
   ...nextVitals,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    "node_modules/**",
+    'node_modules/**',
     '.next/**',
     'out/**',
     'build/**',
     'next-env.d.ts',
   ]),
 ])
-export default eslintConfig;
+ 
+export default eslintConfig
