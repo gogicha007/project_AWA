54613a6de7b57ae7709097ccb2a34c6d98eb522e
gogicha007 <gogicha@gmail.com>
2025-10-30 17:51:29 +0400

    chor: save the progress

--- frontend/src/components/projects/project-boq/components/IdentifyColumns.tsx
+++ frontend/src/components/projects/project-boq/components/IdentifyColumns.tsx
@@ -1,14 +1,22 @@
-'use client'
+'use client';
 
-import React from 'react'
-import styles from 'modal.module.css'
+import React from 'react';
+import styles from 'modal.module.css';
 
 const Identifycolumns = () => {
   return (
     <div>
-        Identifycolumns
+      <table>
+        <tr>
+          <th>unidenitified</th>
+        </tr>
+        <tr>
+          <td>data</td>
+        </tr>
+      </table>
+      Identifycolumns
     </div>
-  )
-}
+  );
+};
 
-export default Identifycolumns
+export default Identifycolumns;
